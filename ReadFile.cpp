732c645a092f9dd8f7090e777c45637694ae5b57
EmmanuelFlorencioT <emm.ftr2@gmail.com>
2022-09-08 12:25:04 -0500

    Can now read vertices, read faces and pair vertices to those faces

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -35,17 +35,27 @@ class Vertex{
     }
 };
 
-class SquareFace{
+class Face{
     private:
-        Vertex a, b, c, d;
+        int nVer = 0;
+        vector <Vertex> ver;
+    
+    public:
+        void setNumVer(int n){
+            nVer = n;
+        }
+        void addVertex(Vertex data){
+            ver.push_back(data);
+        }
+        void checkVertices(){
+            for(int i = 0; i < nVer; i++){
+                cout << "V" << i+1 << ":\n";
+                ver[i].checkLocation();
+            }
+        }
 };
 
-class TriangleFace{
-    private:
-        Vertex a, b, c;
-};
-
-void getCoords(string line, Vertex &v){
+void readVertexCoords(string line, Vertex &v){
     int begin = 2, j;
     string buff;
 
@@ -59,26 +69,64 @@ void getCoords(string line, Vertex &v){
     }
 }
 
+void readFaceVertices(string line, vector<int> &index){
+    int begin = 2, j;
+    string buff;
+
+    while(begin < line.size()){
+        for(j = begin; (line[j] != ' ' && j < line.size()); j++){
+            buff+=line[j];
+        }
+        index.push_back(stoi(buff)-1);
+        buff.clear();
+        begin = j + 1;
+    }
+}
+
+void pairFaceVertices(vector<Vertex> ver, vector<int> index, Face &f){
+    f.setNumVer(index.size());
+    for(int i = 0; i < index.size(); i++){
+        f.addVertex(ver[index[i]]);
+    }
+}
+
+void printRealIndex(vector<int> index){
+    for(int i = 0; i < index.size(); i++){
+        cout << index[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(){
     fstream myFile;
     myFile.open("Cubo.obj", ios::in);
     vector <Vertex> vertices;
+    vector <Face> faces;
 
     if(myFile.is_open()){
         string s;
         while(getline(myFile, s)){
             if(s[0] == 'v'){
-                Vertex temp;
-                getCoords(s, temp);
-                vertices.push_back(temp);
+                Vertex tempV;
+                readVertexCoords(s, tempV);
+                vertices.push_back(tempV);
+            }
+            if(s[0] == 'f'){
+                vector <int> indices;
+                Face tempF;
+                readFaceVertices(s, indices);
+                // printRealIndex(indices);
+                pairFaceVertices(vertices, indices, tempF);
+                faces.push_back(tempF);
             }
         }
 
         myFile.close();
     }
 
-    for(int i = 0; i < vertices.size(); i++){
-        cout << "Vertex " << i << endl;
-        vertices[i].checkLocation();
+    for(int i = 0; i < faces.size(); i++){
+        cout << "Face " << i << "\n";
+        faces[i].checkVertices();
+        cout << "----------------\n";
     }
 }
